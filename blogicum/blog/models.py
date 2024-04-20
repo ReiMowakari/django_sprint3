817bddabd2b75aba18895b1618782fbffb08679f
Aleksandr Kovalev <ReiMowakari@yandex.ru>
2024-04-20 13:45:42 +0300

    first trying

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -67,8 +67,8 @@ class Post(BaseModel):
     text = models.TextField(verbose_name='Текст')
     pub_date = models.DateTimeField(
         verbose_name='Дата и время публикации',
-        help_text='''Если установить дату и время в будущем — 
-        можно делать отложенные публикации.'''
+        help_text='''Если установить дату и время в будущем
+        — можно делать отложенные публикации.'''
     )
 
     author = models.ForeignKey(
