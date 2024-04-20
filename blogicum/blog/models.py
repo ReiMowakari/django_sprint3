58d5bc6d18364efa0d53b14909f76ac24e8a5260
Aleksandr Kovalev <ReiMowakari@yandex.ru>
2024-04-20 13:43:02 +0300

    first trying

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -31,7 +31,8 @@ class Category(BaseModel):
     slug = models.SlugField(
         unique=True,
         verbose_name='Идентификатор',
-        help_text='Идентификатор страницы для URL; разрешены символы латиницы, цифры, дефис и подчёркивание.'
+        help_text='''Идентификатор страницы для URL;
+        разрешены символы латиницы, цифры, дефис и подчёркивание.'''
     )
 
     class Meta:
@@ -66,7 +67,8 @@ class Post(BaseModel):
     text = models.TextField(verbose_name='Текст')
     pub_date = models.DateTimeField(
         verbose_name='Дата и время публикации',
-        help_text='Если установить дату и время в будущем — можно делать отложенные публикации.'
+        help_text='''Если установить дату и время в будущем — 
+        можно делать отложенные публикации.'''
     )
 
     author = models.ForeignKey(
